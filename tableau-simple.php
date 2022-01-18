0d981c05bf02ed50c1940d5dc362b6e8a606680d
Clemence Perrouin <clemence.perrouin2020@campus-eni.fr>
2022-01-18 16:21:18 +0100

    tp-tableau-simle : étape 1 à 6

---
+++ tableau-simple.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+
+    // 1- Créer un tableau d’entier allant de 0 à 41 en PHP en une seule ligne
+    $numbers = range(0, 41);
+
+    // 2- Mélanger ce tableau
+    shuffle($numbers);
+
+    // 3- Créer un tableau de 16 entiers aléatoires à partir de l’ETAPE 2 en une ligne en PHP
+    $random_numbers = array_slice($numbers, 0, 16);
+
+    //4- Créer un tableau de 32 paires d’entiers aléatoires à partir de l’ETAPE 3 en une ligne en PHP
+    $pair_random_numbers = array_merge($random_numbers, $random_numbers);
+
+    //5- Mélanger ce tableau de 32 lignes
+    shuffle($pair_random_numbers);
+
+    //6- Parcourir ce tableau pour afficher les images (ex:1.jpg)
+        //-> jeux.php
+
+?>
